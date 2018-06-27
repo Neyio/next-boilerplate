3dbcb4dc4a64f4f18560f434bdfd6087320b722d
xy.yang <xy.yang@ctrip.com>
2018-06-27 17:53:08 +0800

    Test

--- components/AddCount.js
+++ components/AddCount.js
@@ -7,15 +7,24 @@ import common from '../styles/common.less'
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 class AddCount extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      num: 1
+    }
+  }
   add = () => {
     this.props.addCount()
   }
-
+  addnum = () => {
+    this.setState({
+      num: this.state.num + 1
+    })
+  } 
   render () {
+    console.log('渲染')
     const { count } = this.props
     const images = [
-      
-      
       {
         original: 'https://wallhalla.com/thumbs/preview/v/vaDwJNrS7AE.jpg',
         thumbnail: 'https://wallhalla.com/thumbs/preview/v/vaDwJNrS7AE.jpg'
@@ -27,7 +36,8 @@ class AddCount extends Component {
     ]
     return (
       <div className={common.example}>
-         
+        <span>Num: {this.state.num}</span>
+         <button onClick={() => this.addnum()}>+num</button>
         <h1 >AddCount: <span>{count}</span></h1>
         <button onClick={this.add}>Add To Count</button>
         <ImageGallery items={images} />
